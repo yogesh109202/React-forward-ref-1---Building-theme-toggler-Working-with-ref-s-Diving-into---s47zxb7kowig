6a64c552468ba70f23eea831daccf5587fa1f3bd
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-22 18:57:12 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,28 +1,28 @@
 'use client'
-import React,{useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 import InputField from './InputField'
 
 function Home() {
 
-  const newRef=useRef();
-  const [values,setValues] = useState("")
+  const newRef = useRef();
+  const [values, setValues] = useState("")
 
   const settingValue = () => {
-
+    setValues(newRef.current.value)
   }
 
   const focusInput = () => {
-
+    newRef.current.focus()
   }
 
 
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/>
-    <br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
+      <InputField ref={newRef} type="text" />
+      <br />
+      <button id="settingValueButton" onClick={settingValue}>Set Value</button>
+      <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br /><br />
+      <textarea id="textarea" value={values}></textarea>
 
     </div>
   )
